1b428c6ee9448117b00bfba6d288376b8b00ee91
Ajith R B <ajithrb2001@gmail.com>
2021-08-04 13:21:07 +0530

    first commit

---
+++ H.py
@@ -0,0 +1,22 @@
+
+#Name:Abhijith R B
+#program: To draw heart using turtle
+#Date:4-8-2021
+#wednesday
+
+
+
+
+
+from turtle import *
+
+color ('red') #function for color
+begin_fill()  #function for draw begin
+pensize(3)    #functon for line thickness
+left(50)      #change direction
+forward(133)  #to move turtle forward
+circle(50,200)#draw circle
+right(140)    #change direction
+circle(50,200)#draw circle
+forward(133)  #to move turtle forward
+end_fill()    #draw cose
